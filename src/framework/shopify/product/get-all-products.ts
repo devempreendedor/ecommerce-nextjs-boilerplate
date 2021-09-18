21f5252bca1add0d785ce1601b710478969b9d02
Lucas Couto <lucas@orbithub.com.br>
2021-09-18 16:56:51 -0300

    normaliza products

--- src/framework/shopify/product/get-all-products.ts
+++ src/framework/shopify/product/get-all-products.ts
@@ -1,5 +1,22 @@
-const getAllProducts = async (): Promise<number[]> => {
-  const products = [1, 2, 3]
+import getAllProductsQuery from '../utils/queries/get-all-products'
+import fetchApi from '../utils/fetch-api'
+
+import { ProductConnection } from '../schema'
+
+type ReturnType = {
+  products: ProductConnection
+}
+
+const getAllProducts = async (): Promise<any> => {
+  const { data } = await fetchApi<ReturnType>({
+    query: getAllProductsQuery
+  })
+
+  const products =
+    data.products.edges.map(({ node: product }) => {
+      return product
+    }) ?? []
+
   return products
 }
 
