3a1d848ad1ffe98ec612425fb284628b2f563501
Lucas Couto <lucas@orbithub.com.br>
2021-09-18 20:23:57 -0300

    refactor: reusable configuration with framework specific configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')
+const { withFrameworkConfig } = require('./src/framework/common/config')
 
 const isProd = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  reactStrictMode: true,
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
+module.exports = withPWA(
+  withFrameworkConfig({
+    reactStrictMode: true,
+    pwa: {
+      dest: 'public',
+      disable: !isProd
+    },
+    i18n: {
+      locales: ['pt-BR', 'en-US'],
+      defaultLocale: 'pt-BR'
+    }
+  })
+)
+
+console.log('next.config.js', JSON.stringify(module.exports))
